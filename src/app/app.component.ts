44662d5afe41023b333fae88c7741013cbcf00dd
thisLinda <la.for.lizzi@gmail.com>
2022-12-22 13:55:03 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+// https://www.youtube.com/watch?v=3dHNOWTI7H8
+// https://github.com/bradtraversy/angular-crash-2021/blob/main/src/styles.css
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tracker';
 }
